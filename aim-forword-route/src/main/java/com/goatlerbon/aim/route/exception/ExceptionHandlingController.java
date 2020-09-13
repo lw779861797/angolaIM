caa9ad6a9f2e2ec52995a4996dabb61fa41aceb2
lw779861797 <779861797@qq.com>
2020-09-13 20:35:24 +0800

    路由服务器 的 全局异常处理

--- aim-forword-route/src/main/java/com/goatlerbon/aim/route/exception/ExceptionHandlingController.java
+++ aim-forword-route/src/main/java/com/goatlerbon/aim/route/exception/ExceptionHandlingController.java
@@ -1,4 +1,28 @@
 package com.goatlerbon.aim.route.exception;
 
+import com.goatlerbon.aim.common.exception.AIMException;
+import com.goatlerbon.aim.common.res.BaseResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @ControllerAdvice 是增强的 controller
+ * 使用 @ControllerAdvice 实现全局异常处理
+ */
+@ControllerAdvice
 public class ExceptionHandlingController {
+    private static Logger logger = LoggerFactory.getLogger(ExceptionHandlingController.class) ;
+
+    @ExceptionHandler(AIMException.class)
+    @ResponseBody
+    public BaseResponse handleAllExceptions(AIMException ex) {
+        logger.error("exception", ex);
+        BaseResponse baseResponse = new BaseResponse();
+        baseResponse.setCode(ex.getErrorCode());
+        baseResponse.setMessage(ex.getMessage());
+        return baseResponse ;
+    }
 }
